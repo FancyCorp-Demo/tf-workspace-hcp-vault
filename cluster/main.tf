2433fc1b6595b3404392885ffe3efb1ddbdbd7ef
Lucy Davinhart <lucy@lucymhdavies.com>
2023-04-05 20:12:17 +0100

    Switch out to AWS for HCP Vault
    It's quicker, and right now I'm linking it in to TFC which means I need
    it to be publicly accessible anyway.

--- cluster/main.tf
+++ cluster/main.tf
@@ -27,9 +27,14 @@ provider "hcp" {}
 module "hcp-vault" {
   source = "./hcp-vault"
 
-  name       = "azure-london"
-  cloud      = "azure"
-  region     = "uksouth"
+  #  name       = "azure-london"
+  #  cloud      = "azure"
+  #  region     = "uksouth"
+
+  name   = "aws-london"
+  cloud  = "aws"
+  region = "eu-west-2"
+
   cidr_block = "10.0.0.0/24"
 
   public_endpoint = true
