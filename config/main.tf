99e230c3b8a30bf5d569047f94f3e046e0822af6
Lucy Davinhart <lucy@lucymhdavies.com>
2023-04-11 18:20:09 +0100

    Remove test lookup-self

--- config/main.tf
+++ config/main.tf
@@ -8,14 +8,3 @@ terraform {
     }
   }
 }
-
-
-# As a test, lookup self
-
-data "vault_generic_secret" "lookup_self" {
-  path = "auth/token/lookup-self"
-}
-
-output "self" {
-  value = nonsensitive(data.vault_generic_secret.lookup_self.data)
-}
