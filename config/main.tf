977767f877dc0d77a9c48e4cb045536fa3d7ad04
Lucy Davinhart <lucy@lucymhdavies.com>
2023-08-11 17:22:22 +0100

    Move Azure config to its own workspace

--- config/main.tf
+++ config/main.tf
@@ -7,17 +7,4 @@ terraform {
       name = "vault-config"
     }
   }
-
-  # Minimum provider version for OIDC auth
-  required_providers {
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = ">= 2.29.0"
-    }
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.25.0"
-    }
-
-  }
 }
--- config/tfc.tf
+++
@@ -1,1 +0,0 @@
-# TODO: Create some additional OIDC auth roles for TFC to use to auth
